70f9ddbf06b152ae50756ca57fda9ac8ad373ce4
nzukie <nzukie.b@husky.neu.edu>
2021-12-01 10:47:07 -0500

    Start of candidate election

--- raft/constants.py
+++ raft/constants.py
@@ -1,3 +1,4 @@
+# MSG KEYS
 SRC = 'src'
 DST = 'dst'
 LEADER = 'leader'
@@ -6,9 +7,14 @@ MID = 'MID'
 KEY = 'key'
 VALUE = 'value'
 
+# TYPES
 GET = 'get'
 PUT = 'put'
 OK = 'ok'
 FAIL = 'fail'
 REDIRECT = 'redirect'
 NOOP = 'noop'
+ELECT = 'election'
+
+# Range for election timeout
+TIMEOUT_RANGE = range(150, 500)
